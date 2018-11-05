184280f58e1412ead2ad84c7ca93c4cdf225c299
Luca Montanari <luca.montanari@manageddesigns.it>
2018-11-05 12:02:40 +0100

    First release

---
+++ src/Alexa.NET.Extensions.Lambda/Builders/SpeechBuilder.cs
@@ -0,0 +1,58 @@
+﻿using Alexa.NET.Extensions.Types;
+using Alexa.NET.Response;
+using Alexa.NET.Response.Ssml;
+using System;
+using System.Collections.Generic;
+
+namespace Alexa.NET.Lambda.Builders
+{
+    public static class SpeechBuilder
+    {
+        public static PlainTextOutputSpeech CreatePlainTextOutputSpeech(string text)
+        {
+            return new PlainTextOutputSpeech { Text = text };
+        }
+
+        public static SsmlOutputSpeech CreateSsmlOutputSpeech(IEnumerable<ISsml> elements, int breakMilliseconds = 300)
+        {
+            if (elements == null)
+            {
+                throw new ArgumentNullException(nameof(elements));
+            }
+
+            var speech = new Speech();
+
+            speech.Elements.AddRange(elements);
+
+            var ssml = speech.ToXml();
+
+            return new SsmlOutputSpeech { Ssml = ssml };
+        }
+
+        public static SsmlOutputSpeech CreateSsmlOutputSpeech(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+
+            var speech = new Speech();
+
+            speech.Elements.Add(new PlainText(text));
+
+            var ssml = speech.ToXml();
+
+            return new SsmlOutputSpeech { Ssml = ssml };
+        }
+
+        public static Emphasis CreateSsmlEmphasis(string text, string emphasisLevel = EmphasisLevel.Moderate)
+        {
+            return new Emphasis(text) { Level = emphasisLevel };
+        }
+
+        public static Break CreateSsmlBreak(int time, string timeUnit = TimeUnit.Milliseconds)
+        {
+            return new Break { Time = time + timeUnit };
+        }
+    }
+}
---
+++ src/Alexa.NET.Extensions.Lambda/Serialization/Serializer.cs
@@ -0,0 +1,43 @@
+﻿using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.IO;
+
+namespace Alexa.NET.Lambda.Serialization
+{
+    public class Serializer : ILambdaSerializer
+    {
+        private readonly JsonSerializer InternalSerializer;
+
+        public Serializer()
+        {
+            this.InternalSerializer = new JsonSerializer
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            this.InternalSerializer.Converters.Add(new StringEnumConverter
+            {
+                AllowIntegerValues = false
+            });
+        }
+
+        public void Serialize<T>(T response, Stream responseStream)
+        {
+            var writer = new StreamWriter(responseStream);
+            this.InternalSerializer.Serialize(writer, response);
+            writer.Flush();
+        }
+
+        public T Deserialize<T>(Stream requestStream)
+        {
+            using (var reader = new StreamReader(requestStream))
+            {
+                using (var jsonReader = new JsonTextReader(reader))
+                {
+                    return this.InternalSerializer.Deserialize<T>(jsonReader);
+                }
+            }
+        }
+    }
+}
